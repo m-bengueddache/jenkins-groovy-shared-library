ee130705b7a5bc773df45ab5269fac49e94303e1
Mohamed <bengueddache.mohamed@gmail.com>
2026-06-18 10:22:23 +0200

    feat: add dockerLogin and dockerPush methods to Docker class

--- src/com/example/Docker.groovy
+++ src/com/example/Docker.groovy
@@ -1,4 +1,4 @@
-#!/usr/bin/env groovy
+#!/user/bin/env groovy
 package com.example
 
 class Docker implements Serializable {
@@ -12,5 +12,15 @@ class Docker implements Serializable {
     def buildDockerImage(String imageName) {
         script.echo "building the docker image..."
         script.sh "docker build -t $imageName ."
+        }
+
+    def dockerLogin() {
+        script.withCredentials([script.usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin"
+        }
+    }
+
+    def dockerPush(String imageName) {
+        script.sh "docker push $imageName"
     }
 }
